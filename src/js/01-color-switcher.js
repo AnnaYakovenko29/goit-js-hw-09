38434edf3abc226ddad30499592f622cf31962c2
AnnaYakovenko29 <125920571+AnnaYakovenko29@users.noreply.github.com>
2023-04-26 21:03:16 +0300

    package.json

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,10 @@
 
+const startBtn = document.querySelector(data-start);
+const stopBtn = document.querySelector(data-stop);
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        console.log(`I love async JS!  ${Math.random()}`);
+      }, 1000);
+    });
