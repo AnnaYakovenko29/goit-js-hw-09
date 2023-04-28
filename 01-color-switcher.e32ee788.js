1050c10cf8ad60e88f22168e4dcad7d32abb96cc
AnnaYakovenko29 <125920571+AnnaYakovenko29@users.noreply.github.com>
2023-04-28 20:50:35 +0000

    Deploying to gh-pages from @ AnnaYakovenko29/goit-js-hw-09@b6fa229b44ff1199be3607cfcdec10c413625ddf 🚀

--- 01-color-switcher.e32ee788.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(data-start);document.querySelector(data-stop);let t=null;e.addEventListener("click",(()=>{t=setInterval((()=>{console.log(`I love async JS!  ${Math.random()}`)}),1e3)}));
-//# sourceMappingURL=01-color-switcher.e32ee788.js.map
